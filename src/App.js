c07e6d3bff9f794dbb6ee0ec73af8f4d054cc6c3
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-30 22:05:54 +0800

    Link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeDefinitions)
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
+  { path: '/products', element: <Products /> },
 ])
 
 function App() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Home = () => {
-  return (
-    <div>Home</div>
+    return (
+      <>
+            <div>Home</div>
+            <p>Go to <Link to="/products">Products</Link></p>
+      </>
+    
   )
 }
 
