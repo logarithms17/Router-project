e626b08e24881a32dcc9c59b5609c313d4a0da4a
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-30 21:45:15 +0800

    added gitignore

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+createBrowserRouter([
+  { path: '/', element: <Home /> },
+  {}
+])
+
 function App() {
   return <div></div>;
 }
